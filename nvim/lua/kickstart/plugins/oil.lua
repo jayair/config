cb0f688e25b8e62a4c4ab6dad47bffde1538f5e0
Jay V <air@live.ca>
2024-12-19 12:56:32 -0500

    tweaking oil

--- nvim/lua/kickstart/plugins/oil.lua
+++ nvim/lua/kickstart/plugins/oil.lua
@@ -3,7 +3,14 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      watch_for_changes = true,
+      keymaps = {
+        ['<C-s>'] = false,
+        ['<C-h>'] = false,
+        ['<C-t>'] = false,
+      },
+    },
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
